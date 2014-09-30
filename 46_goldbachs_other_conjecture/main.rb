7d8a904343cf47d5ca9470f77431f8cd03575d9a
Nick Slocum <nslocum@gmail.com>
2014-09-30 15:58:19 -0400

    Solved 46.

---
+++ 46_goldbachs_other_conjecture/main.rb
@@ -0,0 +1,22 @@
+require 'prime'
+
+odd_composite_nums =
+  Enumerator.new do |y|
+    n = 3
+    loop do
+      n += 2 while n.prime?
+      y.yield n
+      n += 2
+    end
+  end
+
+result =
+  odd_composite_nums.find do |c|
+    max_s = Math.sqrt(c).to_i
+    (1..max_s).find { |s|
+      p = c - 2 * s**2
+      p.prime? && p > 0
+    } == nil
+  end
+
+puts result
