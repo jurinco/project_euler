d693e725463f27ca9f9f7aa5e8f647ee3020690b
Nick Slocum <nslocum@gmail.com>
2014-10-03 19:33:17 -0400

    Refactored solution and simplified testing.

---
+++ 3_prime_factors/main.rb
@@ -0,0 +1,11 @@
+require 'prime'
+
+def largest_prime_factor(n)
+  return nil if n.prime?
+  n.prime_division.map { |arr| arr[0] }.max
+end
+
+# Test Cases
+p [largest_prime_factor(13_195), largest_prime_factor(13_195) == 29]
+
+p largest_prime_factor(600_851_475_143)
--- 3_prime_factors/prime_factors_spec.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'prime_factors'
-
-describe 'prime_factors' do
-  it 'returns the correct values' do
-    expect(prime_factors(13195)).to eql([5, 7, 13, 29])
-  end
-end
