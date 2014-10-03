8d4dc749d416a7cd78c508b240d93bd788aeaf15
Nick Slocum <nslocum@gmail.com>
2014-10-03 18:49:24 -0400

    Housekeeping.

--- 1_multiples_of_3_and_5/multiples.rb
+++ 1_multiples_of_3_and_5/multiples.rb
@@ -1,18 +1,3 @@
-# def multiples(limit=999)
-#   sum = 0
-#   (1...limit).each do |n|
-#     if (n % 3 == 0) || (n % 5 == 0)
-#       sum += n
-#     end
-#   end
-#   sum
-# end
-
-# def multiples(limit=999)
-#   (1...limit).reduce(0) { |sum, n|sum + ((n % 3 == 0 || n % 5 == 0) ? n : 0) }
-# end
-
-
 def multiples(limit=999)
   (1...limit).reduce(0) do |sum, n|
     if (n % 3 == 0 || n % 5 == 0)
