a193a165c085184a4c268a7a3c40b3ce157dc8c9
Nick Slocum <nslocum@gmail.com>
2014-10-03 18:49:01 -0400

    Refactor.

--- 1_multiples_of_3_and_5/multiples.rb
+++ 1_multiples_of_3_and_5/multiples.rb
@@ -1,13 +1,24 @@
-def multiples(limit=999)
-  sum = 0
-  (1...limit).each do |n|
-    if (n % 3 == 0) || (n % 5 == 0)
-      sum += n
-    end
-  end
-  return sum
-end
+# def multiples(limit=999)
+#   sum = 0
+#   (1...limit).each do |n|
+#     if (n % 3 == 0) || (n % 5 == 0)
+#       sum += n
+#     end
+#   end
+#   sum
+# end
 
 # def multiples(limit=999)
 #   (1...limit).reduce(0) { |sum, n|sum + ((n % 3 == 0 || n % 5 == 0) ? n : 0) }
 # end
+
+
+def multiples(limit=999)
+  (1...limit).reduce(0) do |sum, n|
+    if (n % 3 == 0 || n % 5 == 0)
+      sum + n
+    else
+      sum + 0
+    end
+  end
+end
