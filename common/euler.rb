e9edfba59060a3215c33ee24272d4aae6b963bed
Nick Slocum <nslocum@gmail.com>
2014-10-04 11:34:24 -0400

    Removed old implemtation of factors().

--- common/euler.rb
+++ common/euler.rb
@@ -35,19 +35,6 @@ module Euler
     n.factorial / ( k.factorial * (n-k).factorial )
   end
 
-  def factors(n)
-    f_list = []
-    step = n.even? ? 1 : 2
-    upper_bound = Math.sqrt(n).to_i
-    (1..upper_bound).step(step).each { |i|
-      if n % i == 0
-        f_list << i
-        f_list << n / i unless i * i == n
-      end
-    }
-    f_list.sort
-  end
-
   def number_with_underscore_delimiter(n)
     arr = n.to_s.split('')
     new_num = arr.pop(3)
