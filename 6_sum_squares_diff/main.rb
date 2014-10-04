559d6b07d1212bd8a3cfaff4beed55d4a2eea317
Nick Slocum <nslocum@gmail.com>
2014-10-03 20:54:31 -0400

    Cleaned up solution 6 and simplified testing.

--- 6_sum_squares_diff/main.rb
+++ 6_sum_squares_diff/main.rb
@@ -1,4 +1,23 @@
-require_relative 'squares'
+# Square every values in an array, then sum the results
+def sum_of_squares(vals)
+  vals.map { |v| v ** 2 }.reduce(:+)
+end
+
+# Sum the values in an array, then square the result
+def square_of_sum(vals)
+  vals.reduce(:+) ** 2
+end
+
+def sum_square_diff(vals)
+  square_of_sum(vals) - sum_of_squares(vals)
+end
+
+# Test Cases
+vals = (1..10).to_a
+p [sum_of_squares(vals), sum_of_squares(vals) == 385]
+p [square_of_sum(vals), square_of_sum(vals) == 3025]
+p [sum_square_diff(vals), sum_square_diff(vals) == 2640]
+
 
 vals = (1..100).to_a
-puts sum_square_diff(vals)
+puts sum_square_diff(vals) # 25164150
