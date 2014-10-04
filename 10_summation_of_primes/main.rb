c5e395648603441bc88fff0f494d7a58ad6a87af
Nick Slocum <nslocum@gmail.com>
2014-10-03 21:34:26 -0400

    Simplified file structure of solution 10.

--- 10_summation_of_primes/main.rb
+++ 10_summation_of_primes/main.rb
@@ -1,3 +1,10 @@
-require_relative 'summation_of_primes'
+require 'prime'
 
-puts summation_of_primes(2_000_000)
+def summation_of_primes(limit)
+  Prime.take_while { |p| p < limit }.reduce(:+)
+end
+
+# Test Case
+p [summation_of_primes(10), summation_of_primes(10) == 17]
+
+p summation_of_primes(2_000_000) # 142913828922
--- 10_summation_of_primes/summation_of_primes.rb
+++
@@ -1,5 +0,0 @@
-require 'prime'
-
-def summation_of_primes(limit)
-  Prime.take_while { |p| p < limit }.reduce(:+)
-end
