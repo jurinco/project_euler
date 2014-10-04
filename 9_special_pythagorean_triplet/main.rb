5412f50df6ec9c7be5cc6f0819e6074c0b6323f2
Nick Slocum <nslocum@gmail.com>
2014-10-03 21:31:48 -0400

    Refactored solution 9 and added a test case.

--- 9_special_pythagorean_triplet/main.rb
+++ 9_special_pythagorean_triplet/main.rb
@@ -1,11 +1,30 @@
-require_relative 'pythagorean_triplets'
+# Find three natural numbers, a < b < c, for which
+# a^2 + b^2 = c^2
+# c_max: max value for c
+pythagorean_triplets = Enumerator.new do |y|
+  c = 3
+  loop do
+    c2 = c ** 2
+    ((c/2).ceil..c-1).each do |b|
+      b2 = b ** 2
+      a2 = c2 - b2
+      a = Math.sqrt(a2)
+      if (a % 1 == 0) && (a < b)
+        y.yield [a,b,c].map{ |n| n.to_i }
+      end
+    end
+    c += 1
+  end
+end
 
-triplets = pythagorean_triplets(500)
+# Test Case
+p [pythagorean_triplets.first, pythagorean_triplets.first == [3,4,5]]
 
-triplets.each do |triplet|
+pythagorean_triplets.each do |triplet|
   if triplet.reduce(:+) == 1000
     puts triplet.join(' ')
     puts "    sum: #{triplet.reduce(:+)}"
-    puts "product: #{triplet.reduce(:*)}"
+    puts "product: #{triplet.reduce(:*)}" # => 31875000
+    break
   end
 end
