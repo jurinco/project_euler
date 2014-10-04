31b902dbcf75621bd15c046f460e6683a51d4aa3
Nick Slocum <nslocum@gmail.com>
2014-10-04 18:35:32 -0400

    Refactored 22.

--- 22_name_scores/main.rb
+++ 22_name_scores/main.rb
@@ -1,20 +1,17 @@
-names = []
-File.open('names.txt', 'r') do |f|
-  f.each_line do |line|
-    names = line
-  end
-end
-
-names = names.gsub('"','').split(',').sort
+names = File.read('names.txt')
+names = names.scan(/\w+/).sort!
 
 def sum_of_chars(str)
-  sum = 0
-  str.each_codepoint { |c| sum += c-64 }
-  sum
+  str.each_codepoint.reduce(0) { |sum,char| sum + char-64 }
 end
 
-total = 0
+# Test Cases
+p [names[937], names[937] == 'COLIN']
+p [sum_of_chars(names[937]), sum_of_chars(names[937]) == 53]
+
+sum = 0
 names.each_with_index do |name, i|
-  total += (i+1) * sum_of_chars(name)
+  sum += (i+1) * sum_of_chars(name)
 end
-puts "total: #{total}"
+
+p sum # => 871198282
