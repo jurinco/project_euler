5f9d0509800cb66ba0ce938666d74587b9cc259d
Nick Slocum <nslocum@gmail.com>
2014-10-03 22:07:56 -0400

    Refactored solution 16 and added test case.

--- 16_power_digit_sum/main.rb
+++ 16_power_digit_sum/main.rb
@@ -1,1 +1,8 @@
-(2**1000).to_s.split('').reduce(0) { |sum, d| sum + d.to_i } # => 1366
+def sum_of_digits_of_two_to_the_n_power(n)
+  (2**n).to_s.split('').reduce(0) { |sum, d| sum + d.to_i }
+end
+
+# Test Case
+p [sum_of_digits_of_two_to_the_n_power(15), sum_of_digits_of_two_to_the_n_power(15) == 26]
+
+puts sum_of_digits_of_two_to_the_n_power(1000) # => 1366
