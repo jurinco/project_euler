adf73306dfedf9ca83dc45ac1d7bc97a6e492134
Nick Slocum <nslocum@gmail.com>
2014-10-03 23:24:56 -0400

    Refactored 20 and added test case.

--- 20_factorial_digit_sum/main.rb
+++ 20_factorial_digit_sum/main.rb
@@ -1,4 +1,12 @@
 require_relative '../common/euler'
 include Euler
 
-puts factorial(100).to_s.split('').reduce(0) { |sum, d| sum + d.to_i }
+def sum_digits(n)
+  n.to_s.split('').reduce(0) { |sum, d| sum + d.to_i }
+end
+
+# Test Case
+n = factorial(10)
+p [sum_digits(n), sum_digits(n) == 27]
+
+p sum_digits(factorial(100)) # => 648
