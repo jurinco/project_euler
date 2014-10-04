12caac0011ddc633184664a0a172f959ff806584
Nick Slocum <nslocum@gmail.com>
2014-10-03 23:00:05 -0400

    Refactored and simplified 17.

--- 17_number_letter_counts/main.rb
+++ 17_number_letter_counts/main.rb
@@ -1,9 +1,41 @@
-require_relative 'number_to_words'
+def number_to_words(n)
+  return 'zero' if n == 0
+  nums = %w(nil one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen)
+  sig = %w(nil nil twenty thirty forty fifty sixty seventy eighty ninety)
 
-words =
-  (1..999).map { |n|
-    number_to_words(n)
-  }
+  result = []
+  if n >= 1000
+    result << "#{nums[n/1000]} thousand"
+    n %= 1000
+    result << 'and' if n > 0
+  end
 
-words << 'one thousand'
-puts words.reduce(0) { |sum, w| sum + w.gsub(' ', '').length }
+  if n >= 100
+    result << "#{nums[n/100]} hundred"
+    n %= 100
+    result << 'and' if n > 0
+  end
+
+  if n >= 20
+    result << sig[n/10]
+    n %= 10
+  end
+
+  if n > 0
+    result << nums[n]
+  end
+
+  result.join(' ')
+end
+
+def sum_range_of_words(range)
+  words = range.map { |n| number_to_words(n) }
+  words.reduce(0) { |sum, w| sum + w.gsub(' ', '').length }
+end
+
+# Test Cases
+p [number_to_words(342), number_to_words(342) == 'three hundred and forty two']
+p [number_to_words(115), number_to_words(115) == 'one hundred and fifteen']
+p [sum_range_of_words(1..5), sum_range_of_words(1..5) == 19]
+
+p sum_range_of_words(1..1000) # => 21124
