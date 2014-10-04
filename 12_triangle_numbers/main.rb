4ee0d00b011dd9864d8a305eef9db0a456591818
Nick Slocum <nslocum@gmail.com>
2014-10-03 21:49:37 -0400

    Cleaned up solution 12 and added a test case.

---
+++ 12_triangle_numbers/main.rb
@@ -0,0 +1,24 @@
+require_relative '../common/euler'
+
+include Euler
+
+@triangular_nums = Enumerator.new do |y|
+  index = 0
+  sum = 0
+  loop do
+    index += 1
+    sum += index
+    y.yield sum
+  end
+end
+
+def first_triangle_num_with_more_than_n_divisors(n)
+  @triangular_nums.find { |t|
+    factors(t).length > n
+  }
+end
+
+# Test Case
+p [first_triangle_num_with_more_than_n_divisors(5), first_triangle_num_with_more_than_n_divisors(5) == 28]
+
+p first_triangle_num_with_more_than_n_divisors(500) # => 76576500
