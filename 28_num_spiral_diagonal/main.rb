20e18e3a60197306a39d19b260385640abb7687f
Nick Slocum <nslocum@gmail.com>
2014-10-05 13:31:36 -0400

    Refactored 28 and added test cases.

--- 28_num_spiral_diagonal/main.rb
+++ 28_num_spiral_diagonal/main.rb
@@ -1,17 +1,23 @@
-dim = 1001
-
-m = 2
-corner_count = 0
-sum = 0
-(1..dim**2).each do |n|
-  if (n - 1) % m == 0
-    sum += n
-    corner_count += 1
-    if corner_count == 4
-      m += 2
-      corner_count = 0
+def sum_spiral_diagonals(dim)
+  m = 2
+  corner_count = 0
+  sum = 0
+  (1..dim**2).each do |n|
+    if (n - 1) % m == 0
+      sum += n
+      corner_count += 1
+      if corner_count == 4
+        m += 2
+        corner_count = 0
+      end
     end
   end
+  sum
 end
 
-puts "sum: #{sum}"
+# Test Cases
+p [sum_spiral_diagonals(1), sum_spiral_diagonals(1) == 1]
+p [sum_spiral_diagonals(3), sum_spiral_diagonals(3) == 25]
+p [sum_spiral_diagonals(5), sum_spiral_diagonals(5) == 101]
+
+p sum_spiral_diagonals(1001) # => 669171001
