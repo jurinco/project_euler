9511894c074c2c6a37eb9aea088937c11b10686d
Nick Slocum <nslocum@gmail.com>
2014-10-04 22:07:45 -0400

    Refactored 24 to run faster.

--- 24_lexicographic_permutations/main.rb
+++ 24_lexicographic_permutations/main.rb
@@ -1,2 +1,8 @@
 a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-puts a.permutation.to_a[999_999].map { |d| d.to_s }.join
+
+a.permutation.each_with_index do |p, i|
+  if i == 1_000_000-1
+    puts p.join.to_i # => 2783915460
+    break
+  end
+end
