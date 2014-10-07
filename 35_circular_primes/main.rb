0097a40dbaa5610ff81d960c7f610d640052070a
Nick Slocum <nslocum@gmail.com>
2014-10-06 21:17:20 -0400

    Refactored 35 and added test case.

--- 35_circular_primes/main.rb
+++ 35_circular_primes/main.rb
@@ -1,27 +1,23 @@
-require 'benchmark'
 require 'prime'
 
-puts Benchmark.measure {
-  def rotations(n)
-    m = n.to_s.length - 1 # magnitude
+def rotations(n)
+  r = n.to_s.length - 1 # num of rotations
 
-    rotations = []
-    m.times do
-      n0 = n / 10**m
-      n_remain = n % 10**m
-      n = n_remain * 10 + n0
-      rotations << n
-    end
-    rotations
+  r.times.map do
+    n0 = n / 10**r
+    n_remain = n % 10**r
+    n = n_remain * 10 + n0
   end
+end
 
-  vals = []
-  Prime.each do |n|
-    break if n >= 1_000_000
-    rotations = rotations(n)
-    vals << n unless rotations.detect { |r| !r.prime? }
-  end
+vals = []
+Prime.each do |n|
+  break if n >= 1_000_000
+  rotations = rotations(n)
+  vals << n unless rotations.find { |r| !r.prime? }
+end
 
-  puts vals
-  puts "count: #{vals.length}"
-}
+# Test Case
+p [197, vals.include?(197) == true]
+
+p vals.length # => 55
