2dfe76da2e7781f3054f71968510bf17e35c0232
Nick Slocum <nslocum@gmail.com>
2014-10-07 16:16:58 -0400

    Refactored 47 and added test cases.

--- 47_distinct_prime_factors/main.rb
+++ 47_distinct_prime_factors/main.rb
@@ -1,22 +1,15 @@
 require 'prime'
 
-# # 2 consecutive nums with 2 distinct prime factors
-# (1..1_000).find { |n|
-#   n.prime_division.length == 2 && n.next.prime_division.length == 2
-# }
+# Return the first n consecutive numbers with n distinct prime factors
+def consec_nums(n)
+  i = 1
+  loop do
+    return (i...i+n).to_a if (i...i+n).all? { |x| x.prime_division.length == n }
+    i += 1
+  end
+end
 
+p [consec_nums(2), consec_nums(2) == [14,15]]
+p [consec_nums(3), consec_nums(3) == [644,645,646]]
 
-# # 3 consecutive nums with 3 distinct prime factors
-# (1..1_000).find { |n|
-#   n.prime_division.length == 3 &&
-#     (n+1).prime_division.length == 3 &&
-#     (n+2).prime_division.length == 3
-# }
-
-
-puts (1..1_000_000).find { |n|
-  n.prime_division.length == 4 &&
-    (n+1).prime_division.length == 4 &&
-    (n+2).prime_division.length == 4 &&
-    (n+3).prime_division.length == 4
-}
+p consec_nums(4).first # => 134043
