7a0e0dc46477912135414decdaad26ab1c7cac8e
Nick Slocum <nslocum@gmail.com>
2014-10-07 15:53:31 -0400

    Cleaned up 44.

--- 44_pentagon_nums/main.rb
+++ 44_pentagon_nums/main.rb
@@ -1,27 +1,23 @@
-require 'benchmark'
+lim = 10_000
 
-puts Benchmark.measure {
-  lim = 10_000
-  @pantagonals = {}
-  (1..lim).each do |n|
-    p = n * (3*n-1) / 2
-    @pantagonals[p] = true
-  end
-  @p_keys = @pantagonals.keys
+@pantagonals = {}
+(1..lim).each do |n|
+  p = n * (3*n-1) / 2
+  @pantagonals[p] = true
+end
+@p_keys = @pantagonals.keys
 
-  def find_first(lim)
-    (1..lim-1).each do |i|
-      p1 = @p_keys[i]
-      (i+1..lim-1).each do |j|
-        p2 = @p_keys[j]
-        return [p1, p2] if @pantagonals[p1+p2] && @pantagonals[p2-p1]
-      end
+def first_pentagonal_pair(lim)
+  (1..lim-1).each do |j|
+    p1 = @p_keys[j]
+    (j+1..lim-1).each do |k|
+      p2 = @p_keys[k]
+      return [p1, p2] if @pantagonals[p1+p2] && @pantagonals[p2-p1]
     end
-    nil
   end
+  nil
+end
 
-  p1, p2 = find_first(lim)
-  puts "p1: #{p1}"
-  puts "p2: #{p2}"
-  puts "sum: #{p1+p2}"
-}
+p1, p2 = first_pentagonal_pair(lim)
+
+puts p2 - p1 # => 5482660
