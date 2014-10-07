72eb53ead0430ae2992d694cdb40360032de6ea7
Nick Slocum <nslocum@gmail.com>
2014-10-07 15:58:12 -0400

    Refactored 45.

--- 45_tri_pent_and_hex_num/main.rb
+++ 45_tri_pent_and_hex_num/main.rb
@@ -2,12 +2,11 @@ require_relative '../common/euler'
 include Euler
 
 n = 286
-result = nil
-until result do
-  x = triangular_num(n)
-  if x.pentagonal? && x.hexagonal?
-    result = n
-    puts "x: #{x}"
+loop do
+  t = triangular_num(n)
+  if t.pentagonal? && t.hexagonal?
+    puts t # => 1533776805
+    break
   end
   n += 1
 end
