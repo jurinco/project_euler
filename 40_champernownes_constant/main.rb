b3550664048dc4b8f7db478c3425ae5f8bdd20bc
Nick Slocum <nslocum@gmail.com>
2014-10-07 12:39:21 -0400

    Refactored 40 and added test case.

--- 40_champernownes_constant/main.rb
+++ 40_champernownes_constant/main.rb
@@ -1,8 +1,11 @@
 d = (1..190_000).map(&:to_s).join
 
+# Test Case
+p [d[0..32], d[0..32] == '123456789101112131415161718192021']
+
 product = 1
 (0..6).each { |e|
   product *= d[10**e-1].to_i
 }
 
-puts "product: #{product}"
+p product # => 210
