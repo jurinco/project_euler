7b49f4aed1dc502ec8061231d3f64410e9a1f6cb
Nick Slocum <nslocum@gmail.com>
2014-10-10 11:32:41 -0400

    Refactored 59.

--- 59_XOR_decryption/main.rb
+++ 59_XOR_decryption/main.rb
@@ -1,16 +1,22 @@
 cipher = File.read('cipher.txt')
 cipher = cipher.strip.split(',').map(&:to_i)
 
+def decrypt(key, cipher)
+  char_ascii_codes = (32..126)
+  cipher.each_with_index.map { |c,i|
+    d = (c ^ key[i%3])
+    return false unless char_ascii_codes.include?(d)
+    d.chr
+  }.join
+end
+
 common_words = ['and', 'the', 'that']
 
 ('aaa'..'zzz').each do |i|
   key = i.bytes
-  plaintext = cipher.each_with_index.map { |l,i|
-    (l ^ key[i%3]).chr
-  }.join
-  if common_words.all? { |w| plaintext.include?(w) }
+  plaintext = decrypt(key, cipher)
+  if plaintext && common_words.all? { |w| plaintext.include?(w) }
     sum = plaintext.bytes.reduce(:+)
-    p plaintext[0..50], sum
+    p plaintext[0..50], sum # => 107359
   end
 end
-
