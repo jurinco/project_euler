768e37baf78f24319349050f1eeff2e05f5abf83
Nick Slocum <nslocum@gmail.com>
2014-10-12 20:27:20 -0400

    Refactored 62.

--- 62_cubic_permutations/main.rb
+++ 62_cubic_permutations/main.rb
@@ -4,36 +4,30 @@ class Integer
   end
 end
 
-
-def get_cubes(size)
+def cube_permutations(target_count)
   cubes = Hash.new { |h,k| h[k] = [] }
-
   n = 3
+  size = 2
   loop do
     c = n**3
     digits_count = c.num_of_digits
     if digits_count > size
-      break
-    elsif digits_count >= size
-      key = c.to_s.chars.sort.join.to_i
-      cubes[key] << c
+      cubes.clear
+      size += 1
+    end
+
+    key = c.to_s.chars.sort.join.to_i
+    cubes[key] << c
+    if cubes[key].count == target_count
+      return cubes[key].sort
     end
     n += 1
   end
-  cubes
 end
 
-def cube_permutations(count)
-  n = 3
-  loop do
-    cubes = get_cubes(n)
-    result = cubes.select { |k,v| v.count == count }
-    return result.values.map { |vals| vals.sort }.min unless result.empty?
-    n += 1
-  end
-end
 
 # Test Case
 p [cube_permutations(3), cube_permutations(3) == [41063625, 56623104, 66430125]]
+p [cube_permutations(5).min, cube_permutations(5).min == 127035954683]
 
-p cube_permutations(5).min # =>
+p cube_permutations(5).min # => 127035954683
