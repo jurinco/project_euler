2334c70ab0d9cad067bb8aeda51ec98dda029425
Nick Slocum <nslocum@gmail.com>
2014-10-12 20:38:48 -0400

    Fixed logic error in 62.

--- 62_cubic_permutations/main.rb
+++ 62_cubic_permutations/main.rb
@@ -12,19 +12,18 @@ def cube_permutations(target_count)
     c = n**3
     digits_count = c.num_of_digits
     if digits_count > size
+      result = cubes.select { |k,v| v.count == target_count }
+      return result.values.min unless result.empty?
+
       cubes.clear
       size += 1
     end
 
     key = c.to_s.chars.sort.join.to_i
     cubes[key] << c
-    if cubes[key].count == target_count
-      return cubes[key].sort
-    end
     n += 1
   end
 end
-
 
 # Test Case
 p [cube_permutations(3), cube_permutations(3) == [41063625, 56623104, 66430125]]
