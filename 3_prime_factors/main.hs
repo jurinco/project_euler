ce32d0c80d7406a7fc55a143f4aed858e83ae001
Nick Slocum <nslocum@gmail.com>
2014-12-07 13:55:40 -0500

    Optimized primeFactors.

--- 3_prime_factors/main.hs
+++ 3_prime_factors/main.hs
@@ -1,12 +1,15 @@
 -- What is the largest prime factor of the number 600851475143 ?
 
-primeFactors :: Integral a => a -> [a]
+primeFactors :: Integer -> [Integer]
 primeFactors n =
   case factor of
     [] -> [n]
     [x] -> x : primeFactors (div n x)
   where
-    factor = take 1 (filter (\ x -> mod n x == 0) [2 .. div n 2])
+    factor = take 1 (filter (\ x -> mod n x == 0) [2 .. intRoot n])
+
+intRoot :: Integer -> Integer
+intRoot = floor . sqrt . fromInteger
 
 -- primeFactors 13195 == [5,7,13,29]
 
