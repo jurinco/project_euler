024f237239d7dcf0f6d44a6cc4c45a56f52c4183
Nick Slocum <nslocum@gmail.com>
2014-12-11 14:32:58 -0500

    Refactored.

--- 2_even_fibonacci_numbers/main.hs
+++ 2_even_fibonacci_numbers/main.hs
@@ -3,5 +3,5 @@ fibStream = fib 1 2
     fib a b = a : fib b (a+b)
 -- take 10 fibStream == [1,2,3,5,8,13,21,34,55,89]
 
-sol = sum . takeWhile (< 4000000) . filter (\ x -> mod x 2 == 0) $ fibStream
+sol = sum $ takeWhile (< 4000000) $ filter (\ x -> mod x 2 == 0) fibStream
 -- sol == 4613732
