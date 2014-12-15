a3e4dcd76372fe7cdcf1968fde5296533a3ef4a6
Nick Slocum <nslocum@gmail.com>
2014-12-15 13:48:15 -0500

    Reorganized solution 004.

--- haskell/004_largest_palindrome_product.hs
+++ haskell/004_largest_palindrome_product.hs
@@ -1,11 +1,13 @@
 -- Find the largest palindrome made from the product of two 3-digit numbers.
 
 -- Returns the largest palindrome made from the product of two d-digit numbers
-solve d = maximum [p | x <- [lb..ub], y <- [x..ub], let p = x * y, palindrome (show p)]
+largestPalindromeProduct d = maximum [p | x <- [lb..ub], y <- [x..ub], let p = x * y, palindrome (show p)]
   where
     lb = 10 ^ (d - 1)   -- lower bound
     ub = 10 ^ d - 1     -- upper bound
     palindrome s = s == reverse s
 
--- solve 2 == 9009
--- solve 3 == 906609
+test0 = largestPalindromeProduct 1 == 9
+test1 = largestPalindromeProduct 2 == 9009
+
+solution = largestPalindromeProduct 3 -- 906609
--- ruby/004_largest_palindrome_product.rb
+++ ruby/004_largest_palindrome_product.rb
@@ -1,8 +1,9 @@
 # Find the largest palindrome made from the product of two n-digit numbers.
+
 # Return the two digits and their product
 def largest_palindrome_product(num_of_digits)
   upper_lim = 10 ** num_of_digits - 1
-  lower_lim =  10 ** (num_of_digits - 1)
+  lower_lim = 10 ** (num_of_digits - 1)
   result = [1,1,1] # [x, y, product]
 
   upper_lim.downto(lower_lim).each do |x|
