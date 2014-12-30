12b86c36efc6c3d21c53435971a5b48070207a85
Nick Slocum <nslocum@gmail.com>
2014-12-30 16:19:32 -0500

    Minor improvement.

--- haskell/008_largest_product_in_a_series/008.hs
+++ haskell/008_largest_product_in_a_series/008.hs
@@ -7,9 +7,8 @@ import Data.List
 maxProductInStream n = do
   contents <- readFile "data.txt"
   let stream = map digitToInt . concat . lines $ contents
-  let windows = take (length stream - n) . map (take n) . tails $ stream
-  let products = map product windows
-  print $ maximum products
+  let windows = map (take n) . tails $ stream
+  print . maximum . map product $ windows
 
 -- maxProductInStream 4 -- 5832
 -- maxProductInStream 13 -- 23514624000
